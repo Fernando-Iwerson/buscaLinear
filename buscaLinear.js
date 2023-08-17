9e7ad950344cde9fe5af780708ba89ec253abc9d
fernando-iwerson <fernandoiwerson@gmail.com>
2023-08-17 16:00:50 -0300

    first commit

---
+++ buscaLinear.js
@@ -0,0 +1,23 @@
+// Busca Linear
+
+
+function buscaLinear(array, elemntoBuscado) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === elemntoBuscado) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+const array = [15, 8, 10, 25, 12, 30, 5, 20, 18, 7];
+const elementoProcurado = 20;
+
+const elementoEncontrado = buscaLinear(array, elementoProcurado);
+
+if(elementoEncontrado !== -1){
+    console.log(`O elemento ${elementoProcurado} foi encontrado na casa: ${elementoEncontrado}`)
+}else{
+    console.log(`O elemento ${elementoProcurado} não foi encontrado na lista: `)
+}
